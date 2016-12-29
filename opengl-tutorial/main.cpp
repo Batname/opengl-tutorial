950547a418a34d67a3acb345ceace54ac62aeeee
DenisDubinin <dadubinin@gmail.com>
2016-12-29 20:48:48 +0100

    Initial Commit

---
+++ opengl-tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl-tutorial
+//
+//  Created by apple on 2016-12-29.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
